d224464231122dbf8bbc290b7c547d73564b7340
SilverFlash123 <78094306+SilverFlash123@users.noreply.github.com>
2021-06-17 17:53:02 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,16 @@
+class System{
+
+    constructor(){}
+authenticate(actualCode,enteredCode){
+    text(code,300,300)
+    if (actualCode === enteredCode.toUpperCase)
+    return true
+else
+    return false
+
+}
+    // Add code to authenticate the given code and the access codes.
+
+    
+
+}
